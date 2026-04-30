f8c3e648f64d20797f3cf1d6ca8bcdb437aa43f1
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-04-30 22:39:13 +0200

    Added construction destruction of makeApp

--- src/mainLoop.cpp
+++ src/mainLoop.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 
 namespace miniEngine {
+
+makeApp::makeApp() {}
+makeApp::~makeApp() {}
+
 void makeApp::run() {
   while (!createWindow.shouldClose()) {
     glfwPollEvents();
