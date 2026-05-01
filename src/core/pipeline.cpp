a544b803a317de461dd9699d0fa7e063da3a81a7
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-05-01 14:00:35 +0200

    Added fixed functions for pipeline

--- src/core/pipeline.cpp
+++ src/core/pipeline.cpp
@@ -6,10 +6,14 @@
 #include <fstream>
 
 namespace miniEngine {
-PipelineEngine::PipelineEngine(SetupDevice &device) : device{device} {
-  createGraphicsPipeline();
+PipelineEngine::PipelineEngine(SetupDevice &device,
+                               const PipelineConfigInfo &configInfo)
+    : device{device} {
+  createGraphicsPipeline(configInfo);
 }
 PipelineEngine::~PipelineEngine() {
+  vkDestroyPipeline(device.device(), graphicsPipeline, nullptr);
+  vkDestroyPipelineLayout(device.device(), pipelineLayout, nullptr);
   vkDestroyShaderModule(device.device(), fragShaderModule, nullptr);
   vkDestroyShaderModule(device.device(), vertShaderModule, nullptr);
 }
@@ -30,11 +34,24 @@ std::vector<char> PipelineEngine::readFile(const std::string &filename) {
   return buffer;
 }
 
-void PipelineEngine::createGraphicsPipeline() {
+void PipelineEngine::createGraphicsPipeline(
+    const PipelineConfigInfo &configInfo) {
   auto vertShaderCode = readFile("shaders/fullscreen.spv");
   auto fragShaderCode = readFile("shaders/raymarch.spv");
   vertShaderModule = createShaderModule(vertShaderCode);
   fragShaderModule = createShaderModule(fragShaderCode);
+
+  // pipeline layout
+  VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+  pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+  pipelineLayoutInfo.setLayoutCount = 0;
+  pipelineLayoutInfo.pSetLayouts = nullptr;
+  pipelineLayoutInfo.pushConstantRangeCount = 0;
+
+  if (vkCreatePipelineLayout(device.device(), &pipelineLayoutInfo, nullptr,
+                             &pipelineLayout) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create pipeline layout");
+  }
 
   VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
   vertexInputInfo.sType =
@@ -59,6 +76,34 @@ void PipelineEngine::createGraphicsPipeline() {
   shaderStages[1].flags = 0;
   shaderStages[1].pNext = nullptr;
   shaderStages[1].pSpecializationInfo = nullptr;
+
+  VkPipelineRenderingCreateInfo renderingInfo{};
+  renderingInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
+  renderingInfo.colorAttachmentCount = 1;
+  renderingInfo.pColorAttachmentFormats = &configInfo.colorAttachmentFormat;
+
+  VkGraphicsPipelineCreateInfo pipelineInfo{};
+  pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+  pipelineInfo.pNext = &renderingInfo;
+  pipelineInfo.stageCount = 2;
+  pipelineInfo.pStages = shaderStages;
+
+  pipelineInfo.pVertexInputState = &vertexInputInfo;
+  pipelineInfo.pInputAssemblyState = &configInfo.inputAssemblyInfo;
+  pipelineInfo.pViewportState = &configInfo.viewportInfo;
+  pipelineInfo.pRasterizationState = &configInfo.rasterizationInfo;
+  pipelineInfo.pMultisampleState = &configInfo.multisampleInfo;
+  pipelineInfo.pColorBlendState = &configInfo.colorBlendInfo;
+  pipelineInfo.pDynamicState = &configInfo.dynamicStateInfo;
+
+  pipelineInfo.layout = pipelineLayout;
+  pipelineInfo.renderPass = VK_NULL_HANDLE;
+
+  if (vkCreateGraphicsPipelines(device.device(), VK_NULL_HANDLE, 1,
+                                &pipelineInfo, nullptr,
+                                &graphicsPipeline) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create graphics pipeline");
+  }
 }
 
 VkShaderModule
@@ -74,4 +119,60 @@ PipelineEngine::createShaderModule(const std::vector<char> &code) {
   }
   return shaderModule;
 };
+
+void PipelineEngine::defaultPipelineConfigInfo(PipelineConfigInfo &configInfo) {
+  // dynamic state
+  configInfo.dynamicStateEnables = {VK_DYNAMIC_STATE_VIEWPORT,
+                                    VK_DYNAMIC_STATE_SCISSOR};
+  configInfo.dynamicStateInfo.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+  configInfo.dynamicStateInfo.pDynamicStates =
+      configInfo.dynamicStateEnables.data();
+  configInfo.dynamicStateInfo.dynamicStateCount =
+      static_cast<uint32_t>(configInfo.dynamicStateEnables.size());
+  configInfo.dynamicStateInfo.flags = 0;
+
+  // assembly
+  configInfo.inputAssemblyInfo.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+  configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+  configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+  // view port
+  configInfo.viewportInfo.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+  configInfo.viewportInfo.viewportCount = 1;
+  configInfo.viewportInfo.pViewports = nullptr;
+  configInfo.viewportInfo.scissorCount = 1;
+  configInfo.viewportInfo.pScissors = nullptr;
+
+  // Rasterization
+  configInfo.rasterizationInfo.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+  configInfo.rasterizationInfo.depthClampEnable = VK_FALSE;
+  configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+  configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+  configInfo.rasterizationInfo.lineWidth = 1.0f;
+  configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
+  configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+  configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
+
+  // Multisampling
+  configInfo.multisampleInfo.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+  configInfo.multisampleInfo.sampleShadingEnable = VK_FALSE;
+  configInfo.multisampleInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+  // Color Blending
+  configInfo.colorBlendAttachment.colorWriteMask =
+      VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+      VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+  configInfo.colorBlendAttachment.blendEnable = VK_FALSE;
+
+  configInfo.colorBlendInfo.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+  configInfo.colorBlendInfo.logicOpEnable = VK_FALSE;
+  configInfo.colorBlendInfo.attachmentCount = 1;
+  configInfo.colorBlendInfo.pAttachments = &configInfo.colorBlendAttachment;
+}
 } // namespace miniEngine
--- src/core/pipeline.hpp
+++ src/core/pipeline.hpp
@@ -4,20 +4,40 @@
 #include "setup.hpp"
 
 namespace miniEngine {
+struct PipelineConfigInfo {
+  PipelineConfigInfo(const PipelineConfigInfo &) = delete;
+  PipelineConfigInfo &operator=(const PipelineConfigInfo &) = delete;
+
+  VkPipelineViewportStateCreateInfo viewportInfo;
+  VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+  VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+  VkPipelineMultisampleStateCreateInfo multisampleInfo;
+  VkPipelineColorBlendAttachmentState colorBlendAttachment;
+  VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+  VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+  std::vector<VkDynamicState> dynamicStateEnables;
+  VkPipelineDynamicStateCreateInfo dynamicStateInfo;
+  VkFormat colorAttachmentFormat;
+  VkPipelineLayout pipelineLayout = nullptr;
+};
 class PipelineEngine {
 public:
-  PipelineEngine(SetupDevice &device);
+  PipelineEngine(SetupDevice &device, const PipelineConfigInfo &configInfo);
   ~PipelineEngine();
 
   PipelineEngine(const PipelineEngine &) = delete;
   PipelineEngine &operator=(const PipelineEngine &) = delete;
 
+  static void defaultPipelineConfigInfo(PipelineConfigInfo &configInfo);
+
 private:
   SetupDevice &device;
   static std::vector<char> readFile(const std::string &filepath);
-  void createGraphicsPipeline();
+  void createGraphicsPipeline(const PipelineConfigInfo &configInfo);
   VkShaderModule createShaderModule(const std::vector<char> &code);
   VkShaderModule vertShaderModule;
   VkShaderModule fragShaderModule;
+  VkPipeline graphicsPipeline;
+  VkPipelineLayout pipelineLayout;
 };
 } // namespace miniEngine
